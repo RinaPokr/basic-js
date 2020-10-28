fbc328d8a4e9134545329d5f88fd7af7b38e9ae7
Ekaterina Pokras <eapeip@mail.ru>
2020-10-28 19:43:56 +0500

    Commit

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  let res = [];
+ if (Array.isArray(arr)) {
+   
+ for (let index = 0; index < arr.length; index++) {
+   const element = arr[index];
+   switch (element) {
+   case '--discard-next':
+     index++;
+     break;
+   case '--discard-prev':
+   if (arr[index - 2] == '--discard-next') {
+       break;
+     } else {
+       res.pop();
+     }  
+   
+     break;
+   case '--double-next':
+    if (arr[index + 1] !== undefined) {
+      res.push(arr[index + 1]);
+     }
+     break;
+   case '--double-prev':
+     if (arr[index - 2] == '--discard-next') {
+       break;
+     } else {
+     if (arr[index - 1] !== undefined) {  
+       res.push(arr[index - 1]);
+     } 
+     }
+     break;
+   default:
+     res.push(element);
+ }
+   
+ }
+ } else {
+   throw  new Error('Error');
+ }
+ return res;
+ };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  
+  let res = '';
+  if (date === undefined) {
+    res = 'Unable to determine the time of year!';
+  } else if (Object.prototype.toString.call(date) !== '[object Date]') {
+throw new Error('Error');
+   
+  } else {
+
+    let month = date.getMonth();
+    res = (month < 2) ? 'winter' : (month < 5) ? 'spring' : (month < 8) ? 'summer' : (month < 11) ? 'autumn' : 'winter';
+  }
+   
+return res;
 };
